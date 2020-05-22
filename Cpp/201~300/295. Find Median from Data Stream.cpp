c59766cf9cc149d82f0f33ea8ed206d903471434
unknown <you@example.com874288174@qq.com>
2020-05-22 08:36:50 +0800

    fds

--- Cpp/201~300/295. Find Median from Data Stream.cpp
+++ Cpp/201~300/295. Find Median from Data Stream.cpp
@@ -16,3 +16,31 @@ public:
         else return (small.top() - large.top()) / 2.0;
     }
 };
+
+
+
+
+
+class MedianFinder {
+    multiset<int> data;
+    multiset<int>::iterator mid;
+
+public:
+    MedianFinder() {}
+
+    void addNum(int num) {
+        const int n = data.size();
+        data.insert(num);
+        if (!n)                                 // first element inserted
+            mid = data.begin();
+        else if (num < *mid)                    // median is decreased
+            mid = (n & 1 ? prev(mid) : mid);
+        else                                    // median is increased
+            mid = (n & 1 ? mid : next(mid));
+    }
+
+    double findMedian() {
+        const int n = data.size();
+        return ((double) *mid + *next(mid, (n+1) % 2)) * 0.5;
+    }
+};
